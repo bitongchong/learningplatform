4130db96036895a54a2ad67e7542fb25f5d654c5
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-12 19:24:16 +0800

    添加多个接口

--- src/main/java/com/sicau/platform/controller/UserController.java
+++ src/main/java/com/sicau/platform/controller/UserController.java
@@ -1,19 +1,33 @@
 package com.sicau.platform.controller;
 
 import com.sicau.platform.entity.*;
+import com.sicau.platform.exception.EmailSendFailException;
 import com.sicau.platform.service.UserService;
+import com.sicau.platform.util.IdGenerator;
+import com.sicau.platform.util.MailSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Objects;
 
+/**
+ * @author boot liu
+ */
 @RestController
 public class UserController {
     @Autowired
     UserService userService;
     @Autowired
     HostHolder hostHolder;
+    @Autowired
+    MailSender mailSender;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/index")
     public Result index() {
@@ -25,8 +39,8 @@ public class UserController {
         User findUser = userService.findByUserAccount(user.getAccount());
         if (findUser != null) {
             Result result = null;
-            Boolean msg = user.getPassword().equals(findUser.getPassword());
-            if (msg == false) {
+            boolean msg = user.getPassword().equals(findUser.getPassword());
+            if (!msg) {
                 result = new Result(false, StatusCode.LOGINERROR, "密码错误");
             } else {
                 String ticketName = userService.addTicket(findUser.getUserid());
@@ -49,8 +63,9 @@ public class UserController {
 
     @PostMapping("/regist")
     public Result doRegist(@RequestBody User user) {
-        if (userService.findByUserAccount(user.getAccount()) != null)
+        if (userService.findByUserAccount(user.getAccount()) != null) {
             return new Result(false, StatusCode.REGISTERROR, "当前用户已存在");
+        }
         if (userService.addUser(user)) {
             return new Result(true, StatusCode.OK, "用户注册成功");
         } else {
@@ -58,16 +73,54 @@ public class UserController {
         }
     }
 
-/*    *//**
-     * 判断手机号或者email是否正确，identity必须正确（这个id可以是学号，可以是身份证）
-     *//*
     @PostMapping("/forget")
-    public Result findPassword(@RequestBody UserDetail userDetail){
-        userService.
-    }*/
+    public Result findPassword(String account) throws EmailSendFailException {
+        // todo controller层不能写大量逻辑代码，只留下主体流程
+        Long userId = hostHolder.getUser().getUserid();
+        String email = userService.findEmailByUserId(userId);
+        if (Objects.isNull(email)) {
+            return new Result(false, StatusCode.EMAILUNKNOWN, "未完善邮箱信息，请联系管理员进行密码找回");
+        } else {
+            long token = IdGenerator.nextId();
+            Date now = new Date();
+            now.setTime(10 * 60 * 1000 + now.getTime());
+            PasswordToken passwordToken = PasswordToken.builder().token(token).account(hostHolder.getUser()
+                    .getAccount()).status(0).expired(now).build();
+            userService.addPasswordToken(passwordToken);
+            boolean sendEmailResult = mailSender.sendEmail(email, "密码找回邮件", token);
+            if (!sendEmailResult) {
+                throw new EmailSendFailException("邮件发送失败");
+            }
+        }
+        return new Result(true, 0, "密码找回邮件已发送");
+    }
+
+    @GetMapping("/findPassword/{token}")
+    public Result findPassword(@PathVariable("token") Long token) {
+        PasswordToken passwordToken = userService.findPasswordToken(token);
+        if (passwordToken != null) {
+            Date expired = passwordToken.getExpired();
+            if (passwordToken.getStatus() != 0 || new Date().after(expired)) {
+                return new Result(false, StatusCode.TOKENEXPIRED, "token无效");
+            }
+            boolean initPasswordResult = userService.initPassword(passwordToken.getAccount());
+            return initPasswordResult ? new Result(true, StatusCode.OK, "密码已初始化为00000000，登录后请及时更新") :
+                    new Result(false, StatusCode.INTERNALSERVERERROR, "服务端错误");
+        }
+        return new Result(false, StatusCode.TOKENEXPIRED, "token无效");
+    }
+
+    @PostMapping("/changePassword")
+    public Result changePassword(String oldPassword, String newPassword) {
+        if (Objects.equals(hostHolder.getUser().getPassword(), oldPassword)) {
+            userService.changePassword(newPassword);
+            return new Result(true, StatusCode.OK, "密码修改成功");
+        } else {
+            return new Result(false, StatusCode.PASSWORDERRO, "旧密码错误");
+        }
+    }
 
     @PostMapping("/updateDetail")
-    /* todo 这个异常抛得太low了 */
     public Result updateDetail(@RequestBody UserDetail userDetail) throws Exception {
         boolean result = userService.updateUserDetail(userDetail);
         if (result) {
@@ -80,10 +133,10 @@ public class UserController {
     @GetMapping("/getDetail")
     public Result getDetail() {
         UserDetail userDetail = userService.getUserDetail(hostHolder.getUser().getUserid());
-        if (userDetail != null){
+        if (userDetail != null) {
             return new Result(true, StatusCode.OK, "详细信息获取成功", userDetail);
-        }else {
-            return new Result(false, StatusCode.GETINFOEERROR,"你还未设置详细信息");
+        } else {
+            return new Result(false, StatusCode.GETINFOEERROR, "你还未设置详细信息");
         }
     }
 }
