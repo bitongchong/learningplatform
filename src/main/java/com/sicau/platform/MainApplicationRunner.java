07ebe199eaaabf1d2f354196a82304e5e073accb
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-12 14:45:39 +0800

    注释修改

--- src/main/java/com/sicau/platform/MainApplicationRunner.java
+++ src/main/java/com/sicau/platform/MainApplicationRunner.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
- * @author boot liu
+ * @author liuyuehe
  */
 @SpringBootApplication
 @EnableCaching
