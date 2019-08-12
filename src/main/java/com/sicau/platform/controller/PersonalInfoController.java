49be32247b0bb8090a9139868f685e044ccef548
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-12 19:24:30 +0800

    个人信息相关

---
+++ src/main/java/com/sicau/platform/controller/PersonalInfoController.java
@@ -0,0 +1,14 @@
+package com.sicau.platform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuyuehe
+ * @description 个人信息相关的查询
+ * @date 2019/8/12
+ */
+@RestController
+public class PersonalInfoController {
+
+}
