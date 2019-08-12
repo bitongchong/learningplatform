3ae52c306687d0ed07c9f2fe511ca71d4b51b88b
343788619@qq.com <a343788619>
2019-08-12 19:42:54 +0800

    修改bug

--- src/main/java/com/sicau/platform/entity/PasswordToken.java
+++ src/main/java/com/sicau/platform/entity/PasswordToken.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
@@ -23,6 +21,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class PasswordToken {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     String account;
     Long token;
