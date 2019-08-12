6555aef344959990716196b92d854f2405fcd45d
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-12 19:21:38 +0800

    添加Mail发送工具类

---
+++ src/main/java/com/sicau/platform/util/MailSender.java
@@ -0,0 +1,60 @@
+package com.sicau.platform.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author liuyuehe
+ * @description 邮件发送
+ * @date 2019/8/12
+ */
+@Service
+public class MailSender implements InitializingBean {
+    private static final Logger logger = LoggerFactory.getLogger(MailSender.class);
+    private JavaMailSenderImpl mailSender;
+
+    public boolean sendEmail(String to, String subject, Long token) {
+        try {
+            String nick = MimeUtility.encodeText("四川农业大学---密码找回");
+            InternetAddress from = new InternetAddress(nick + "<343788619@qq.com>");
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            // todo 写死的，应添加到apollo中
+            String result = "点击链接修改密码：http://117.50.73.238:8080/findPassword/" + token;
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(result, false);
+            mailSender.send(mimeMessage);
+            return true;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        mailSender = new JavaMailSenderImpl();
+        mailSender.setUsername("343788619@qq.com");
+        mailSender.setPassword("ogocotheqbjubhcj");
+        mailSender.setHost("smtp.qq.com");
+        mailSender.setPort(465);
+        mailSender.setProtocol("smtp");
+        mailSender.setDefaultEncoding("utf8");
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.ssl.enable", true);
+        mailSender.setJavaMailProperties(javaMailProperties);
+    }
+
+}
