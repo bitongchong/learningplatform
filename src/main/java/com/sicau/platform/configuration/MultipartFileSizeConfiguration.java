3a1cfd5adbc97be3ef5993ccc952d1d19bda376a
343788619@qq.com <a343788619>
2019-08-13 16:23:34 +0800

    修改文件最大10M的限制

---
+++ src/main/java/com/sicau/platform/configuration/MultipartFileSizeConfiguration.java
@@ -0,0 +1,24 @@
+package com.sicau.platform.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class MultipartFileSizeConfiguration {
+    @Bean
+    public MultipartConfigElement multipartConfigElement(
+            @Value("${multipart.maxFileSize}") Integer maxFileSize,
+            @Value("${multipart.maxRequestSize}") Integer maxRequestSize) {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 单个文件最大
+        factory.setMaxFileSize(DataSize.ofMegabytes(maxFileSize));
+        // 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.ofMegabytes(maxRequestSize));
+        return factory.createMultipartConfig();
+    }
+}
