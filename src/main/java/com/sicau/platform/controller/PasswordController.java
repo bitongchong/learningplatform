98bcb1d4d7d514465b23539b9d61a7527c39a0cc
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-13 10:31:03 +0800

    抽离密码服务

---
+++ src/main/java/com/sicau/platform/controller/PasswordController.java
@@ -0,0 +1,71 @@
+package com.sicau.platform.controller;
+
+import com.sicau.platform.entity.HostHolder;
+import com.sicau.platform.entity.PasswordToken;
+import com.sicau.platform.entity.Result;
+import com.sicau.platform.entity.StatusCode;
+import com.sicau.platform.exception.EmailSendFailException;
+import com.sicau.platform.service.PasswordService;
+import com.sicau.platform.service.UserService;
+import com.sicau.platform.util.IdGenerator;
+import com.sicau.platform.util.MailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author liuyuehe
+ * @description 密码相关
+ * @date 2019/8/13
+ */
+
+@RestController
+public class PasswordController {
+    @Autowired
+    HostHolder hostHolder;
+    @Autowired
+    UserService userService;
+    @Autowired
+    PasswordService passwordService;
+
+    @PostMapping("/forget")
+    public Result findPassword(String account) throws EmailSendFailException {
+        Long userId = hostHolder.getUser().getUserid();
+        String email = passwordService.findEmailByUserId(userId);
+        if (Objects.isNull(email)) {
+            return new Result(false, StatusCode.EMAILUNKNOWN, "未完善邮箱信息，请联系管理员进行密码找回");
+        }
+        passwordService.sendTokenEmail(userId, email);
+        return new Result(true, 0, "密码找回邮件已发送");
+    }
+
+    @GetMapping("/findPassword/{token}")
+    public Result findPassword(@PathVariable("token") Long token) {
+        PasswordToken passwordToken = passwordService.findPasswordToken(token);
+        if (passwordToken != null) {
+            Date expired = passwordToken.getExpired();
+            if (passwordToken.getStatus() != 0 || new Date().after(expired)) {
+                return new Result(false, StatusCode.TOKENEXPIRED, "token无效或已使用");
+            }
+            boolean initPasswordResult = passwordService.initPassword(passwordToken.getAccount());
+            return initPasswordResult ? new Result(true, StatusCode.OK, "密码已初始化为00000000，登录后请及时更新") :
+                    new Result(false, StatusCode.INTERNALSERVERERROR, "服务端错误");
+        }
+        return new Result(false, StatusCode.TOKENEXPIRED, "token无效");
+    }
+
+    @PostMapping("/changePassword")
+    public Result changePassword(String oldPassword, String newPassword) {
+        if (Objects.equals(hostHolder.getUser().getPassword(), oldPassword)) {
+            passwordService.changePassword(newPassword);
+            return new Result(true, StatusCode.OK, "密码修改成功");
+        } else {
+            return new Result(false, StatusCode.PASSWORDERRO, "旧密码错误");
+        }
+    }
+}
