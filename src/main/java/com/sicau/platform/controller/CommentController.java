9bfd7f854c10bc0bee0daa18ff49a85e0d378a01
343788619@qq.com <a343788619>
2019-08-15 10:51:22 +0800

    bug fix

--- src/main/java/com/sicau/platform/controller/CommentController.java
+++ src/main/java/com/sicau/platform/controller/CommentController.java
@@ -7,11 +7,7 @@ import com.sicau.platform.entity.StatusCode;
 import com.sicau.platform.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +16,7 @@ import java.util.List;
  * @description 为了快速上线，太多地方没有进行参数判断了。。
  * @date 2019/8/14
  */
-@Controller
+@RestController
 public class CommentController {
     @Autowired
     CommentService commentService;
